751d8b80e4a9920d01a89c5fe29387af651f8409
2flo <dianeflozin@gmail.com>
2019-09-05 11:27:42 +0200

    Création d'une entité et d'un repo Ingredient

---
+++ src/Entity/Ingredients.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\IngredientsRepository")
+ */
+class Ingredients
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $Quantity;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $Unity;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?string
+    {
+        return $this->Quantity;
+    }
+
+    public function setQuantity(string $Quantity): self
+    {
+        $this->Quantity = $Quantity;
+
+        return $this;
+    }
+
+    public function getUnity(): ?string
+    {
+        return $this->Unity;
+    }
+
+    public function setUnity(string $Unity): self
+    {
+        $this->Unity = $Unity;
+
+        return $this;
+    }
+}
