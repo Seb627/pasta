c2733630d3fa998b2f91093e1943ec7883a5489b
2flo <dianeflozin@gmail.com>
2019-09-27 16:45:58 +0200

    Ajout d'une relation entre utensil et recipe

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -40,6 +42,21 @@ class Recipe
      * @ORM\Column(type="integer")
      */
     private $Price;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Recipe", inversedBy="recipes")
+     */
+    private $Utensil;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Recipe", mappedBy="Utensil")
+     */
+    private $recipes;
+
+    public function __construct()
+    {
+        $this->recipes = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -102,6 +119,49 @@ class Recipe
     public function setPrice(int $Price): self
     {
         $this->Price = $Price;
+
+        return $this;
+    }
+
+    public function getUtensil(): ?self
+    {
+        return $this->Utensil;
+    }
+
+    public function setUtensil(?self $Utensil): self
+    {
+        $this->Utensil = $Utensil;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getRecipes(): Collection
+    {
+        return $this->recipes;
+    }
+
+    public function addRecipe(self $recipe): self
+    {
+        if (!$this->recipes->contains($recipe)) {
+            $this->recipes[] = $recipe;
+            $recipe->setUtensil($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRecipe(self $recipe): self
+    {
+        if ($this->recipes->contains($recipe)) {
+            $this->recipes->removeElement($recipe);
+            // set the owning side to null (unless already changed)
+            if ($recipe->getUtensil() === $this) {
+                $recipe->setUtensil(null);
+            }
+        }
 
         return $this;
     }
